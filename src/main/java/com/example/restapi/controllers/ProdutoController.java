c90a538361868a96783fa0a1d17aadef73ad1c11
VILSON MORO <vilson.moro@prof.sc.senac.br>
2023-11-27 21:37:30 -0300

    Alteração anotação de endpoints

--- src/main/java/com/example/restapi/controllers/ProdutoController.java
+++ src/main/java/com/example/restapi/controllers/ProdutoController.java
@@ -11,15 +11,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.restapi.entities.Produto;
 
 @RestController
+@RequestMapping("/produtos")
 public class ProdutoController {
 
 	List<Produto> produtos = new ArrayList<Produto>();	
 	
+	/*
 	@GetMapping("/produtos")
 	public ResponseEntity<List<Produto>> getProdutos() {
 		return ResponseEntity.status(HttpStatus.OK).body(produtos);
@@ -75,7 +78,70 @@ public class ProdutoController {
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
 	}
+	
 	@PostMapping("/produtos")
+	public ResponseEntity<Produto> salvarProduto(@RequestBody Produto produto) {
+		produto.setId(produtos.size()+1);
+		produtos.add(produto);
+		return ResponseEntity.status(HttpStatus.CREATED).body(produto);
+	}*/
+	@GetMapping
+	public ResponseEntity<List<Produto>> getProdutos() {
+		return ResponseEntity.status(HttpStatus.OK).body(produtos);
+	}
+	
+	@GetMapping("/{idProduto}")
+	public ResponseEntity<Produto> getProduto(@PathVariable("idProduto") int id) {
+		Produto prod = null;
+		for(Produto pr : produtos) {
+			if(pr.getId() == id) {
+				prod = pr;
+				break;
+			}
+		}
+		if(prod != null)
+		   return ResponseEntity.status(HttpStatus.OK).body(prod);
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+	}
+	
+	@DeleteMapping("/{idProduto}")
+	public ResponseEntity<Produto> excluirProduto(@PathVariable("idProduto") int id) {
+		Produto prod = null;
+		for(Produto pr : produtos) {
+			if(pr.getId() == id) {
+				prod = pr;
+				break;
+			}
+		}
+		if(prod != null) {
+		   produtos.remove(prod);	
+		   return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		}
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+	}
+	
+	@PutMapping("/{idProduto}")
+	public ResponseEntity<Produto> alterarProduto(@PathVariable("idProduto") int id, @RequestBody Produto produto ) {
+		Produto prod = null;
+		for(Produto pr : produtos) {
+			if(pr.getId() == id) {
+				prod = pr;
+				break;
+			}
+		}
+		if(prod != null) {
+		   prod.setDescricao(produto.getDescricao());
+		   prod.setValorUnitario(produto.getValorUnitario());
+		   prod.setEstoque(produto.getEstoque());
+		   return ResponseEntity.status(HttpStatus.OK).body(prod);
+		}
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+	}
+	
+	@PostMapping
 	public ResponseEntity<Produto> salvarProduto(@RequestBody Produto produto) {
 		produto.setId(produtos.size()+1);
 		produtos.add(produto);
